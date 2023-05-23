0564c5c73deb928ff875f93555a3466cb9f5534d
Antonio J Braun <antoniojbraun@gmail.com>
2023-05-22 22:47:21 -0300

    .

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,10 +4,10 @@ import './Footer.css'
 function Footer() {
   return (
     <footer className='container-fluid footer d-flex flex-column align-items-center'>
-      <div className="container d-flex justify-content-center">
+      <div className="d-flex justify-content-center">
             <div className="col p-5">
                 <h4>#Links úteis</h4>
-                <h6>Link 1</h6>
+                <h6 className=''>Link 1</h6>
                 <h6>Link 2</h6>
                 <h6>Link 3</h6>
             </div>
